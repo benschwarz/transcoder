5c4dd4d07d66d13b4152ef125c2497b959d63d3c
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-08 02:12:13 +1100

    Added VERSION constant. Added convenience method Transcoder.for. Required the new YAML parser and Rubygem friendly autoload

--- lib/transcoder.rb
+++ lib/transcoder.rb
@@ -1,13 +1,24 @@
 require 'registry'
 
 module Transcoder
+  VERSION = File.read(File.join(File.dirname(__FILE__), "..", "VERSION"))
+  
   class Parser
     extend Registry
   end
+  
+  def self.for(mime)
+    Parser.for(mime)
+  end
 end
+
+# Autoload that works with Rubygems, 
+# rather than from the current $LOAD_PATH only
+require 'core_ext/autoload'
 
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'transcoder/parser/json'
 require 'transcoder/parser/xml'
 require 'transcoder/parser/feed'
+require 'transcoder/parser/yaml'
