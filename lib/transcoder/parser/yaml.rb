b020d00a485cbde5ce1087535ff7c519d62d0085
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-08 02:10:48 +1100

    Added YAML parser

---
+++ lib/transcoder/parser/yaml.rb
@@ -0,0 +1,14 @@
+autoload :YAML, "yaml"
+
+module Transcoder
+  class Yaml < Parser
+    identifier "text/yaml",
+               "text/x-yaml",
+               "application/yaml",
+               "application/x-yaml"
+    
+    def self.parse(raw)
+      YAML.load(raw)
+    end
+  end
+end
