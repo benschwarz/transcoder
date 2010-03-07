03099d58fcedfc71180ab3fab4cd6243d7186997
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-07 17:04:22 +1100

    Some specs to select the correct class

--- spec/transcoder_spec.rb
+++ spec/transcoder_spec.rb
@@ -1,7 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Transcoder" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "mime-types" do
+    it "should parse json" do
+      Transcoder::Parser.for('application/json').should == Transcoder::Json
+    end
+    
+    it "should parse xml" do
+      Transcoder::Parser.for('text/xml').should == Transcoder::XML
+    end
+    
+    it "should parse rss or atom" do
+      Transcoder::Parser.for('application/atom+xml').should == Transcoder::Feed
+    end
   end
 end
