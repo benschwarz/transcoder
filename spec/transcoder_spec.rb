1750805fec4a5cb54ba4519107c7d5d06be3c69b
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-08 02:12:38 +1100

    Specs to cover new stuff

--- spec/transcoder_spec.rb
+++ spec/transcoder_spec.rb
@@ -1,17 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Transcoder" do
+  it "should alias Transcoder.for to Transcoder::Parser.for" do
+    Transcoder::Parser.should_receive(:for).with("application/json")
+    Transcoder.for("application/json")
+  end
+  
   describe "mime-types" do
     it "should parse json" do
-      Transcoder::Parser.for('application/json').should == Transcoder::Json
+      Transcoder.for('application/json').should == Transcoder::Json
     end
     
     it "should parse xml" do
-      Transcoder::Parser.for('text/xml').should == Transcoder::XML
+      Transcoder.for('text/xml').should == Transcoder::XML
     end
     
     it "should parse rss or atom" do
-      Transcoder::Parser.for('application/atom+xml').should == Transcoder::Feed
+      Transcoder.for('application/atom+xml').should == Transcoder::Feed
+    end
+    
+    it "should parse yaml" do
+      Transcoder.for('application/x-yaml').should == Transcoder::Yaml
     end
   end
 end
