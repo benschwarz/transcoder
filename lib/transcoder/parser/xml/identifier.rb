25688d67d5d0bbc861eac932a85ae3cec5f39a94
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-20 23:25:01 +1100

    Filling out xml generation

--- lib/transcoder/parser/xml/identifier.rb
+++ lib/transcoder/parser/xml/identifier.rb
@@ -1,20 +1,32 @@
 module Transcoder
   class XML < Parser
-    identifier "text/xml",
+    identifier "xml",
+               "text/xml",
                "application/xml"
 
     autoload :GeneratorHandler, "xml/generator_handler"
     autoload :ParserHandler,    "xml/parser_handler"
     
-    def self.generate(objects)
+    def self.generate(object)
+      raise TypeError unless object.is_a? Hash or object.is_a? Array
       builder = XML::GeneratorHandler.new do |xml|
         xml.items {
-          objects.each do |item|
+          if object.is_a? Array
+            object.each{|item| xml.item(:value => item) }
+          elsif object.is_a? Hash
             xml.item {
-              %w(id type class fork).each{|m| item["#{m}_".to_sym] = item.delete(m.to_sym) if item.is_a? Hash }
-            
-              item.each do |k, v|
-                xml.__send__(k, v)
+              object.each do |key, value|
+                # stringify the keys
+                object[key.to_s] = object.delete(key)
+                key = key.to_s
+                
+                # id, type, class and fork must be appended with _
+                if key =~ /(id|type|class|fork)/
+                  object["#{key}_"] = object.delete(key)
+                  key = "#{key}_"
+                end
+              
+                xml.send(key, value)
               end
             }
           end
