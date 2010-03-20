1ff7394dcb6b5666d538f187ba4dc08400b10fc2
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-20 23:24:14 +1100

    Feeds are identified by atom and rss

--- lib/transcoder/parser/feed/identifier.rb
+++ lib/transcoder/parser/feed/identifier.rb
@@ -1,6 +1,8 @@
 module Transcoder
   class Feed < Parser
-    identifier "application/atom+xml",
+    identifier "atom",
+               "rss",
+               "application/atom+xml",
                "application/rss+xml"
     
     autoload :Handler, "feed/handler"
