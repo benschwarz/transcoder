1bd8fd655199c2c0fa98447d4b1263b527e2ea2f
Ben Schwarz <ben.schwarz@gmail.com>
2010-03-20 23:24:48 +1100

    Raise a TypeError if not a hash or an array

--- lib/transcoder/parser/yaml/identifier.rb
+++ lib/transcoder/parser/yaml/identifier.rb
@@ -3,12 +3,14 @@ autoload :YAML, "yaml"
 
 module Transcoder
   class Yaml < Parser
-    identifier "text/yaml",
+    identifier "yaml",
+               "text/yaml",
                "text/x-yaml",
                "application/yaml",
                "application/x-yaml"
     
     def self.generate(objects)
+      raise TypeError unless objects.is_a? Hash or objects.is_a? Array
       YAML.dump(objects)
     end
     
